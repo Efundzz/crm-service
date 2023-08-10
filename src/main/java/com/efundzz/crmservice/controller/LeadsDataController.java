7c6be13ef4b4bbbb3f86277f39116f3c00a8a1d6
Harinadhreddy <harinadh.b@purviewservices.com>
2023-08-10 15:50:30 +0100

    added new controller

--- src/main/java/com/efundzz/crmservice/controller/LeadsDataController.java
+++ src/main/java/com/efundzz/crmservice/controller/LeadsDataController.java
@@ -1,14 +1,15 @@
 package com.efundzz.crmservice.controller;
 
-import com.efundzz.crmservice.DTO.CRMAppliacationResponseDTO;
 import com.efundzz.crmservice.DTO.CRMLeadDataResponseDTO;
 import com.efundzz.crmservice.service.LeadService;
-import com.efundzz.crmservice.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,22 +18,17 @@ import static com.efundzz.crmservice.utils.Brand.determineBrand;
 @RestController
 @RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
-public class ApplicationsController {
-
+public class LeadsDataController {
     @Autowired
-    private LoanService loanService;
-
-    @GetMapping("/applications")
-    public ResponseEntity<List<CRMAppliacationResponseDTO>> getApplications(JwtAuthenticationToken token) {
-        // Get all applications from the database
+    private LeadService leadService;
+    @GetMapping("/leads")
+    public ResponseEntity<List<CRMLeadDataResponseDTO>> getLeadDataByBrand(JwtAuthenticationToken token) {
         List<String> permissions = token.getToken().getClaim("permissions");
         String brand = determineBrand(permissions);
         if (brand == null) {
-            throw new RuntimeException("Invalid permissions"); // Adjust error handling as needed.
+            throw new RuntimeException("Invalid permissions");
         }
         System.out.println(permissions);
-        return ResponseEntity.ok(loanService.getAllLoanDataWithMergedStepData(brand));
+        return ResponseEntity.ok(leadService.getAllLeadDataByBrand(brand));
     }
 }
-
-
