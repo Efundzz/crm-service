9639ebd465b6d9cc18d23977d2aeab58524ea2c1
bhimireddyharinadhreddy <harinadh.b@purviewservices.com>
2023-08-29 10:13:06 +0100

    swager changes

---
+++ src/main/java/com/efundzz/crmservice/configuration/SpringDocConfig.java
@@ -0,0 +1,26 @@
+package com.efundzz.crmservice.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.RedirectView;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig implements WebMvcConfigurer {
+    @Bean
+    public RedirectView redirectSwaggerUi() {
+        return new RedirectView("/swagger-ui.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
