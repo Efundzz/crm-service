698d6db1480e683fc4b40680a70f26fb579982f3
bhimireddyharinadhreddy <harinadh.b@purviewservices.com>
2023-09-14 22:06:32 +0100

    data fixes changes

--- src/main/java/com/efundzz/crmservice/service/LeadStatusService.java
+++ src/main/java/com/efundzz/crmservice/service/LeadStatusService.java
@@ -2,7 +2,9 @@ package com.efundzz.crmservice.service;
 
 import com.efundzz.crmservice.DTO.CRMLeadStatusDTO;
 import com.efundzz.crmservice.entity.LeadStatus;
+import com.efundzz.crmservice.entity.Loan;
 import com.efundzz.crmservice.repository.LeadStatusRepository;
+import com.efundzz.crmservice.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class LeadStatusService {
     @Autowired
     LeadStatusRepository leadStatusRepository;
 
+    @Autowired
+    LoanRepository loanRepository;
+
     public LeadStatus updateLeadStatus(CRMLeadStatusDTO crmLeadStatusDTO) {
         Optional<LeadStatus> optionalLeadStatus = Optional.ofNullable(leadStatusRepository.findByLoanId(crmLeadStatusDTO.getLoanId()));
         if (optionalLeadStatus.isPresent()) {
@@ -25,7 +30,18 @@ public class LeadStatusService {
             leadStatus.setAgentId(crmLeadStatusDTO.getAgentId());
             leadStatus.setComments(crmLeadStatusDTO.getComments());
             leadStatus.setUpdatedAt(LocalDateTime.now());
-            return leadStatusRepository.save(leadStatus);
+
+            leadStatus = leadStatusRepository.save(leadStatus);
+            String loanId = crmLeadStatusDTO.getLoanId();
+            Optional<Loan> optionalLoan = Optional.ofNullable(loanRepository.findById(loanId));
+            if (optionalLoan.isPresent()) {
+                Loan loan = optionalLoan.get();
+                loan.setStatus(crmLeadStatusDTO.getStatus());
+                loanRepository.save(loan);
+            } else {
+                throw new EntityNotFoundException("Loan with ID " + loanId + " not found.");
+            }
+            return leadStatus;
         } else {
             throw new EntityNotFoundException("LeadStatus with ID " + crmLeadStatusDTO.getLoanId() + " not found.");
         }
