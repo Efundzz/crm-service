34c73450ec316352b447583d3caa574124c848b8
bhimireddyharinadhreddy <harinadh.b@purviewservices.com>
2023-10-03 10:19:38 +0100

    Added brand changes

--- src/main/java/com/efundzz/crmservice/utils/Brand.java
+++ src/main/java/com/efundzz/crmservice/utils/Brand.java
@@ -11,6 +11,13 @@ public class Brand {
         permissionsToBrands.put("read:efundzz_applications", "EF");
         permissionsToBrands.put("read:roboloanz_applications", "RL");
         permissionsToBrands.put("read:vahak_leads", "VH");
+        permissionsToBrands.put("read:invoyz_leads", "IV");
+        permissionsToBrands.put("read:raahi_leads", "RB");
+        permissionsToBrands.put("read:knowledge_leads", "KH");
+        permissionsToBrands.put("read:wedezine_leads", "WD");
+        permissionsToBrands.put("read:yoho_leads", "YH");
+        permissionsToBrands.put("read:homelane_leads", "HL");
+        permissionsToBrands.put("read:bymore_leads", "BM");
         permissionsToBrands.put("read:all_applications", "ALL");
         // You can enhance this logic based on your requirements.
         for (String permission : permissions) {
