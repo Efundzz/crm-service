35675a429b839fe85be855b44116eb663ea3b66b
bhimireddyharinadhreddy <harinadh.b@purviewservices.com>
2023-11-06 12:42:52 +0000

    read and write brands

--- src/main/java/com/efundzz/crmservice/controller/BrandAccessController.java
+++ src/main/java/com/efundzz/crmservice/controller/BrandAccessController.java
@@ -1,5 +1,7 @@
 package com.efundzz.crmservice.controller;
 
+import com.efundzz.crmservice.service.BrandAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,23 +11,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.efundzz.crmservice.constants.AppConstants.PERMISSIONS;
-import static com.efundzz.crmservice.utils.Brand.determineBrand;
 
 @RestController
 @RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
 public class BrandAccessController {
 
-    @GetMapping("/accessibleBrand")
-    public ResponseEntity<String> getBrand(JwtAuthenticationToken token) {
+    @Autowired
+    private BrandAccessService brandAccessService;
+    @GetMapping("/readBrands")
+    public ResponseEntity<List<String>> getReadBrands(JwtAuthenticationToken token) {
         List<String> permissions = token.getToken().getClaim(PERMISSIONS);
-        String brand = determineBrand(permissions);
-        if (brand == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        List<String> brands = brandAccessService.determineReadBrands(permissions);
+
+        if (brands.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.emptyList());
         }
-        return ResponseEntity.ok(brand);
+
+        return ResponseEntity.ok(brands);
+    }
+    @GetMapping("/writeBrands")
+    public ResponseEntity<List<String>> getWriteBrands(JwtAuthenticationToken token) {
+        List<String> permissions = token.getToken().getClaim(PERMISSIONS);
+        List<String> brands = brandAccessService.determineWriteBrands(permissions);
+
+        if (brands.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.emptyList());
+        }
+        return ResponseEntity.ok(brands);
     }
 }
