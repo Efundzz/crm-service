67242cd12c7a66decbb08bb2f30effb3c67248cd
bhimireddyharinadhreddy <harinadh.b@purviewservices.com>
2023-11-23 09:42:42 +0000

    brm changes

--- src/main/java/com/efundzz/crmservice/DTO/DecisionResultDTO.java
+++ src/main/java/com/efundzz/crmservice/DTO/DecisionResultDTO.java
@@ -2,6 +2,9 @@ package com.efundzz.crmservice.DTO;
 
 import lombok.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,12 +17,12 @@ public class DecisionResultDTO {
     private double roi;
     private double tenure;
 
-//	public Map<String, Object> toMap() {
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("entity", this.entity);
-//        map.put("proposed emi", this.proposedEmi);
-//        map.put("roi", this.roi);
-//        map.put("tenure", this.tenure);
-//        return map;
-//    }
+	public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("entity", this.entity);
+        map.put("proposedEmi", this.proposedEmi);
+        map.put("roi", this.roi);
+        map.put("tenure", this.tenure);
+        return map;
+    }
 }
